0783d423803a4c5f2071d4ffdfc3300cc565a8d6
Sarna555 <sarnikowski.rafal@gmail.com>
2014-07-07 18:55:16 +0200

    added entry point

---
+++ Admin.Logic/Administration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Security.Principal;
+using System.Security.Permissions;
+using Security;
+
+namespace Admin.Logic
+{
+    public class Administration
+    {
+        [PrincipalPermissionAttribute(SecurityAction.Demand, Role = "ReadUsers")]
+        public static List<UserResult> GetAll()
+        {
+
+            SQLtoLinqDataContext db = new SQLtoLinqDataContext();
+            var userResults = (from p in db.Users
+                               select new UserResult
+                               {
+                                   user_ID = p.user_ID,
+                                   name = p.name,
+                                   surname = p.surname,
+                                   email = p.email
+                               }).ToList<UserResult>();
+            return userResults;
+        }
+    }
+}
