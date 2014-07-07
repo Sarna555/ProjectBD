408300d61b74b0586fecdb2d1ecfa25ff25d0b54
Sarna555 <sarnikowski.rafal@gmail.com>
2014-07-08 00:05:57 +0200

    getting users

--- Admin.Logic/Administration.cs
+++ Admin.Logic/Administration.cs
@@ -12,11 +12,15 @@ namespace Admin.Logic
 {
     public class Administration
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>List of all users</returns>
         [PrincipalPermissionAttribute(SecurityAction.Demand, Role = "ReadUsers")]
-        public static List<UserResult> GetAll()
+        public static List<UserResult> GetAllUsers()
         {
 
-            SQLtoLinqDataContext db = new SQLtoLinqDataContext();
+            var db = new SQLtoLinqDataContext();
             var userResults = (from p in db.Users
                                select new UserResult
                                {
@@ -25,6 +29,23 @@ namespace Admin.Logic
                                    surname = p.surname,
                                    login = p.login
                                }).ToList<UserResult>();
+            foreach(UserResult ele in userResults)
+            {
+                ele.operations = (from o in db.operations
+                                  from u2o in db.users2operations
+                                  from u in db.Users
+                                  where o.operation_ID == u2o.operation_ID && u.user_ID == u2o.user_ID && u.user_ID == ele.user_ID
+                                  select o.name)
+                                .Union
+                                (from o in db.operations
+                                 from g2o in db.groups2operations
+                                 from u2g in db.users2groups
+                                 from u in db.Users
+                                 from g in db.groups
+                                 where u.user_ID == u2g.user_ID && g.group_ID == u2g.group_ID && g.group_ID == g2o.group_ID && o.operation_ID == g2o.operation_ID && u.user_ID == ele.user_ID
+                                 select o.name).ToList<string>();
+                //Nic nie poradze, jak na razie optymalniej nie chce działać :D
+            }
             return userResults;
         }
     }
