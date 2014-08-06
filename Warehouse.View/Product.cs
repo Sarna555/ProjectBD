c0f2fb0ee35fea2b7beba5a8bd953e1eabafcded
rachey <roderyk.raszka@gmail.com>
2014-08-06 14:36:23 +0200

    Changes @ work

--- Warehouse.View/Product.cs
+++ Warehouse.View/Product.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Security;
 
 namespace Warehouse
 {
@@ -16,32 +17,51 @@ namespace Warehouse
         private string switchi;
         public Product(string switchiIn, string palletCodeIn, string name, string category, DateTime bestBefore)
         {
-            switchi = switchiIn;
-            palletCode = palletCodeIn;
-            InitializeComponent();
-            var categorylist = Warehouse.Logic.Warehouse.GetAllCategories();
-            comboBox1.DataSource = categorylist;
-            if (string.Equals(switchi,"exists"))
+            try{
+                switchi = switchiIn;
+                palletCode = palletCodeIn;
+                InitializeComponent();
+                var categorylist = Warehouse.Logic.Warehouse.GetAllCategories();
+                comboBox1.DataSource = categorylist;
+                if (string.Equals(switchi,"exists"))
+                {
+                    this.textBox1.Text = name;
+                    this.dateTimePicker1.Value = bestBefore;
+                    this.comboBox1.SelectedIndex = this.comboBox1.Items.IndexOf(category);
+                }
+            }
+             catch (System.Security.SecurityException se)
             {
-                this.textBox1.Text = name;
-                this.dateTimePicker1.Value = bestBefore;
-                this.comboBox1.SelectedIndex = this.comboBox1.Items.IndexOf(category);
+                MessageBox.Show("Permission denied " + se.Message);
+            }
+            catch (Exception se)
+            {
+                MessageBox.Show(se.Message);
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-           switch (switchi)
-            {
-                case "new":
-                    Warehouse.Logic.Warehouse.AddProduct(palletCode, this.textBox1.Text, this.dateTimePicker1.Value, comboBox1.SelectedItem.ToString());
-                    break;
-                case "exists":
-                    Warehouse.Logic.Warehouse.UpdateProduct(productID, this.textBox1.Text, this.dateTimePicker1.Value, comboBox1.SelectedItem.ToString());
-                    break;
+            try{
+               switch (switchi)
+                {
+                    case "new":
+                        Warehouse.Logic.Warehouse.AddProduct(palletCode, this.textBox1.Text, this.dateTimePicker1.Value, comboBox1.SelectedItem.ToString());
+                        break;
+                    case "exists":
+                        Warehouse.Logic.Warehouse.UpdateProduct(productID, this.textBox1.Text, this.dateTimePicker1.Value, comboBox1.SelectedItem.ToString());
+                        break;
             }
-           this.DialogResult = DialogResult.OK;
-           this.Close();
+            catch (System.Security.SecurityException se)
+            {
+                MessageBox.Show("Permission denied " + se.Message);
+            }
+            catch (Exception se)
+            {
+                MessageBox.Show(se.Message);
+            }
+            this.DialogResult = DialogResult.OK;
+            this.Close();
         }
     }
 }
